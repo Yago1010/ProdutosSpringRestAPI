6243dcc269f87d78227060a41b433e0f37f46bdd
Rafael <rafaelsouzavalle@gmail.com>
2021-08-02 21:55:27 -0300

    Altere pacote das classes

--- src/main/java/com/example/demo/ProdutoRepository.java
+++ src/main/java/com/example/demo/ProdutoRepository.java
@@ -1,9 +1,7 @@
-package com.example.repositories;
+package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
